c5acb23b40098c67da5c8653f5d60b9aa6aedb20
Leonor Albornoz <leonoralbornoz99@gmail.com>
2021-03-17 21:30:36 -0300

    new

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -5,3 +5,8 @@ class RegistroUsuarioForm(UserCreationForm):
     class Meta:
         model = Usuario
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class RegistroUsuarioForm2(UserCreationForm):
+	model= Usuario
+	
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from .forms import RegistroUsuarioForm
+from .forms import RegistroUsuarioForm, RegistroUsuarioForm2 
 
 # Create your views here.
 
 class registro(CreateView):
    form_class = RegistroUsuarioForm
    template_name = 'usuarios/registro.html'
-   success_url = reverse_lazy('login')
+   success_url = reverse_lazy('registrodos')
+
+class registro2(CreateView):
+	form_class = RegistroUsuarioForm2
+	template_name = "usuarios/registroparte2.html"
+	success_url = reverse_lazy('login')
+
