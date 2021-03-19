8fefffc47723fb011cda9f9d35eb970a77c83586
Leonor Albornoz <leonoralbornoz99@gmail.com>
2021-03-19 11:15:57 -0300

    ola

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
+from django import forms
+from apps.usuarios.models import usuario_Nuevo
 
 class RegistroUsuarioForm(UserCreationForm):
     class Meta:
@@ -7,6 +9,7 @@ class RegistroUsuarioForm(UserCreationForm):
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
-class RegistroUsuarioForm2(UserCreationForm):
-	model= Usuario
-	
+class RegistroUsuarioForm2(forms.ModelForm):
+	class Meta:
+		model = usuario_Nuevo
+		fields = ['username', 'email', 'first_name', 'last_name', 'profesion', 'imagen', 'telefono', 'descripcion']
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegistroUsuarioForm, RegistroUsuarioForm2 
+from .models import usuario_Nuevo
 
 # Create your views here.
 
@@ -11,7 +12,7 @@ class registro(CreateView):
    success_url = reverse_lazy('registrodos')
 
 class registro2(CreateView):
+	model = 'usuario_Nuevo'
 	form_class = RegistroUsuarioForm2
-	template_name = "usuarios/registroparte2.html"
+	template_name = 'usuarios/registroparte2.html'
 	success_url = reverse_lazy('login')
-
